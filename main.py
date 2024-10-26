f360819f0669ba34b7cfdc2d983445cdbd14c346
Iloking <halylish@gmail.com>
2024-10-26 15:57:48 +0300

    completed

--- main.py
+++ main.py
@@ -21,3 +21,16 @@ def func2(n):
     return sum
 
 print(func2(5))
+
+# Algorithm - 3
+def func3(n):
+    sum = 0
+    iteration = 0
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            sum = sum + 1
+            iteration += 1
+    print("For Algo 3:", n, "Iteration:", iteration)
+    return sum
+
+print(func3(5))
